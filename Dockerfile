a54d4847aaa29122e3c53ec6eec0deac9c92f922
Mauricio Dioli <madioli26@hotmail.com>
2024-08-01 19:54:45 +0000

    ultimo

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Usa una imagen base oficial de Python
 FROM python:3.12
 
@@ -25,3 +26,32 @@ ENV FLASK_ENV=development
 
 # Comando por defecto para ejecutar la aplicación
 CMD ["flask", "run", "--host=0.0.0.0"]
+=======
+# Usa una imagen base oficial de Python
+FROM python:3.12
+
+# Establece el directorio de trabajo en el contenedor
+WORKDIR /app
+
+# Copia los archivos de requisitos a la imagen
+COPY requirements.txt .
+
+# Instala las dependencias necesarias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Actualiza pyRofex
+RUN pip install -U pyRofex
+
+# Desinstala la librería websocket y luego la vuelve a instalar
+RUN pip uninstall -y websocket && pip install websocket-client
+
+# Copia el contenido de tu proyecto a la imagen
+COPY . .
+
+# Establece las variables de entorno necesarias
+ENV FLASK_APP=app/app.py
+ENV FLASK_ENV=development
+
+# Comando por defecto para ejecutar la aplicación
+CMD ["flask", "run", "--host=0.0.0.0"]
+>>>>>>> 91d15fd (actualizacion)
--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, current_app
 from config.config import Config
 from dotenv import load_dotenv
@@ -91,3 +92,98 @@ def check_db():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
+=======
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, current_app
+from config.config import Config
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from sqlalchemy import create_engine, text
+from sqlalchemy.pool import QueuePool
+from flask_marshmallow import Marshmallow
+import os
+import traceback
+import jwt
+from pyRofex.components.exceptions import ApiException
+import copy
+from app.models.usuario import Usuario
+from app.models.cuentas import Cuenta
+from app.controllers.usuario_controller import usuario_controller
+from app.controllers.operacion_controller import operacion_controller
+from app.controllers.panelControl import panelControl
+from app.controllers.datoSheet import datoSheet
+from app.controllers.wsocket import wsocket
+from app.controllers.instrumentoGet import instrumentoGet
+from app.controllers.validaInstrumentos import validaInstrumentos
+from app.controllers.instrumentos import instrumentos
+from app.tokens.token import token
+from app.controllers.get_login import get_login
+
+
+load_dotenv()
+
+app = Flask(__name__, static_folder='static')
+app.config.from_object(Config)
+
+db = SQLAlchemy()
+db.init_app(app)
+
+CORS(app)
+
+app.config['JWT_SECRET_KEY'] = '621289'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/refresh/'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+jwt = JWTManager(app)
+
+app.register_blueprint(panelControl)
+app.register_blueprint(wsocket)
+app.register_blueprint(datoSheet)
+app.register_blueprint(token)
+app.register_blueprint(instrumentoGet)
+app.register_blueprint(validaInstrumentos)
+app.register_blueprint(instrumentos)
+app.register_blueprint(get_login)
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, poolclass=QueuePool, pool_timeout=60, pool_size=1000)
+
+def configure_db_session(app):
+    with app.app_context():
+        db.session.configure(bind=engine)
+
+configure_db_session(app)
+
+ma = Marshmallow(app)
+
+app.secret_key = '*0984632'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    try:
+      
+        return render_template('index.html')
+    except Exception as e:
+        error_message = f"Error en index: {str(e)}"
+        print(error_message)
+        traceback.print_exc()
+        return f"Error en index: {str(e)}", 500
+
+@app.route('/check_db')
+def check_db():
+    try:
+        result = db.session.execute(text('SELECT 1'))
+        if result.scalar() == 1:
+            return 'Conexión a la base de datos exitosa!'
+        else:
+            return 'No se pudo establecer conexión a la base de datos.'
+    except Exception as e:
+        return f'Error en la conexión a la base de datos: {str(e)}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
+>>>>>>> 91d15fd (actualizacion)
